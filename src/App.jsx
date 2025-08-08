946d687e6722b7e089aa791ca94183fd72b4cf96
Nguyen Ba Dat <datngba2310@gmail.com>
2025-08-08 15:10:37 +0700

    install tailwindcss and style login form.

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,47 @@ function App() {
 
   return (
     <>
-      <div className='sign-in-page-container'>
+      <div className="m-0 p-0 box-border flex flex-col gap-15 bg-auto ">
         {/* head */}
-        <div className="sign-in-form-head-container">
-          <h2 className="sign-in-head-text">Sign in</h2>
+        <div className="flex flex-col items-center">
+          <h2 className="font-bold text-5xl">Sign in</h2>
         </div>
         
         {/* form */}
-        <div className="sign-in-form-container">
+        <div className="">
           <form action={handleSignIn}
-            className="sign-in-form"
+            className="flex flex-col gap-5"
             name='basic-login'>
               <input
                 type='text'
-                className='sign-in-username-field'
+                className="border-1 border-gray-500 p-4 pb-2 pt-2 w-80 rounded-4xl outline-0 focus:border-cyan-300"
                 name={FORM_FIELD.USERNAME}
                 placeholder={PLACEHOLDER_FORM.USERNAME}
               />
 
               <input 
                 type="password"
-                className='sign-in-password-field'
+                className="border-1 border-gray-500 p-4 pb-2 pt-2 w-80 rounded-4xl outline-0 focus:border-cyan-300"
                 name={FORM_FIELD.PASSWORD}
                 placeholder={PLACEHOLDER_FORM.PASSWORD}
               />
 
+              <div className='flex justify-between'>
+                <div className='flex gap-1.5 items-center'>
+                  <input
+                    type="checkbox" 
+                    name="remember"
+                  />
+                  <span>Remember me</span>
+                </div>
+
+                <div className="self-baseline-last underline underline-offset-2">
+                  Forgot Password?
+                </div>
+              </div>
+
               <button 
-                className='sign-in-submit-btn'
+                className="mt-10 p-2 rounded-4xl bg-gray-500 hover:bg-cyan-500 hover:text-black hover:font-medium cursor-pointer"
                 type="submit"
               >
                   Sign in
