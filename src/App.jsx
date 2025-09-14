769541a0d8a74ce98ac28e3ec08cae853e6138ac
Nguyen Ba Dat <datngba2310@gmail.com>
2025-09-14 22:20:13 +0700

    implement basic header, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import SignIn from './pages/Auth/SignIn'
+import Header from './components/layout/public/Header'
+import Footer from './components/layout/public/Footer'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Footer />
     </>
   )
 }
---
+++ src/components/layout/public/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="logo">
+        <img src="" alt="GDC Logo" />
+        <img src="" alt="UoG Logo" />
+      </div>
+
+      <nav>
+        <ul>
+          <li>ABOUT</li>
+          <li>MEMBERS</li>
+          <li>ACTIVITIES</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+
+      <a href="">ACCOUNT</a>
+    </div>
+  )
+}
+
+export default Header
