9fc73bbc4a4f0a1685a3d4656e93a53eec5940b5
Nguyen Ba Dat <datngba2310@gmail.com>
2025-09-17 12:32:25 +0700

    adjust error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import SignIn from './pages/auth/SignIn'
 import Header from './components/layout/public/Header'
 import Footer from './components/layout/public/Footer'
+import ErrorBoundary from './components/layout/public/ErrorBoundary'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
       {/* <Header /> */}
       {/* <Footer /> */}
       <main className='flex items-center justify-center min-h-[46.5vw]'>
-        <SignIn />
+        {/* <SignIn /> */}
+        <ErrorBoundary />
       </main>
     </>
   )
--- src/components/layout/public/ErrorBoundary.jsx
+++ src/components/layout/public/ErrorBoundary.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const ErrorBoundary = () => {
   return (
-    <div>
-      <img src="" alt="404 error image" />
-      <span>PAGE NOT FOUND</span>
-      <p>Lorem ipsum</p>
-      <button>Back to Home Page</button>
+    <div className='bg-white text-black'>
+      <img src="src/assets/items/404.webp" alt="404 image" />
+      <span className='pink-color monument-extra-bold text-3xl'>PAGE NOT FOUND</span>
+      <div>
+        <p>Lorem ipsum</p>
+        <button>Back to Home Page</button>
+      </div>
     </div>
   )
 }
