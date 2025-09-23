66b7fa34ff8acf146fe527421eab0fbf9e5e642b
Nguyen Ba Dat <datngba2310@gmail.com>
2025-09-23 10:28:53 +0700

    Modify a little bit public web footer ui

--- src/components/layout/public/Footer.jsx
+++ src/components/layout/public/Footer.jsx
@@ -9,10 +9,10 @@ import {
 const Footer = () => {
   return (
     <div className="flex flex-col p-2 pl-9 pr-9 gap-6">
-      <img className="w-60" src="src/assets/logos/GDC_logo.svg" alt="GDC Logo" />
-      <div className="flex flex-col items-start md:flex-row md:justify-between md:items-end">
+      <img className="w-60 md:w-55" src="src/assets/logos/GDC_logo.svg" alt="GDC Logo" />
+      <div className="flex flex-col items-start md:flex-row md:justify-between md:items-start">
         <div>
-          <h1 className="monument-regular text-[1.6rem]">
+          <h1 className="monument-regular text-[1.6rem] md:text-3xl">
             Wanna <span className="pink-color">create</span> an
             <br />
             <span className="pink-color"> unforgettable </span>
@@ -25,7 +25,7 @@ const Footer = () => {
           </button>
         </div>
 
-        <div className="flex flex-col justify-baseline mt-10 gap-6">
+        <div className="flex flex-col justify-baseline mt-10 md:mt-0 gap-6">
           <div className="flex flex-col text-xl items-start gap-1 ">
             <h3 className="pink-color font-medium">CONTACT</h3>
             <p>@gdc.fpt2022@gmail.com</p>
@@ -51,7 +51,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex flex-col mt-1 gap-6">
+      <div className="flex flex-col mt-1 md:mt-8 gap-6">
         <p className="border-b-1 text-[12.5px]">&copy; GDC - Greenwich Dance Crew</p>
         <img src="src/assets/items/footer.webp" alt="Footer image" />
       </div>
