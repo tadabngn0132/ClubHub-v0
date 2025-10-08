3abab177668d4160a52a366ed4290bdafd352f29
Nguyen Ba Dat <datngba2310@gmail.com>
2025-10-08 23:21:22 +0700

    Implement member page ui

--- src/pages/public/Members.jsx
+++ src/pages/public/Members.jsx
@@ -1,6 +1,29 @@
 const Members = () => {
   return (
-    <div className="flex flex-col min-h-[var(--pub-main-min-height)] mt-[var(--pub-main-margin-y)] mb-[var(--pub-main-margin-y)] md:pl-[var(--pub-container-padding-x)] md:pr-[var(--pub-container-padding-x)]">Members Page</div>
+    <div className="flex flex-col min-h-[var(--pub-main-min-height)] mt-[var(--pub-main-margin-y)] mb-[var(--pub-main-margin-y)]">
+      <div className="flex flex-col items-center md:pl-[var(--pub-container-padding-x)] md:pr-[var(--pub-container-padding-x)] mt-10 gap-8">
+        <div className="flex w-full gap-6">
+          <span className="monument-extra-bold uppercase text-[7rem] text-[var(--pink-color)] leading-20">Green</span>
+          <div className="bg-[#212121] w-full"></div>
+        </div>
+
+        <div className="flex w-full gap-6">
+          <div className="bg-[#212121] w-full"></div>
+          <div className="flex flex-col">
+            <span className="monument-extra-bold uppercase text-[7rem] text-[var(--pink-color)] leading-20">Wich</span>
+            <span className="w-7/12">Lorem ipsum dolor sit amet, consectetuer adipiscing elit</span>
+          </div>
+        </div>
+
+        <div className="flex w-full gap-6">
+          <span className="monument-extra-bold uppercase text-[7rem] text-[var(--pink-color)] leading-20">Dance</span>
+          <div className="bg-[#212121] w-full"></div>
+          <span className="monument-extra-bold uppercase text-[7rem] text-[var(--pink-color)] leading-20">Crew</span>
+        </div>
+
+        <span className="w-8/12 text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing.....</span>
+      </div>
+    </div>
   )
 }
 
