782a1a0385616a4cdd96dd88fc8312ddc019c056
Nguyen Ba Dat <datngba2310@gmail.com>
2025-12-21 19:12:43 +0700

    feat: update layout and styling for Home and Activities pages; enhance CSS variables for better responsiveness

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -4,7 +4,6 @@ import { useDispatch } from "react-redux"
 import { loginAction } from '../../store/slices/authSlice'
 import { login } from '../../services/authService'
 import { useState } from "react"
-import Dashboard from "../private/admin/AdminDashboard.jsx"
 
 const SignIn = () => {
   const {
@@ -42,178 +41,182 @@ const SignIn = () => {
   }
 
   return (
-    <div className="flex justify-between bg-black w-full md:w-9/12 lg:w-full xl:w-11/12 2xl:w-10/12 rounded-[3rem] relative">
-      <div className="hidden lg:flex lg:flex-col lg:p-10 lg:pt-8 lg:justify-between">
-        <video
-          className="object-cover lg:w-2/3 2xl:w-7/10 h-full absolute top-0 left-0 z-[0] rounded-[3rem] rounded-r-none opacity-75 animate-scale-bigger animate-slide-left"
-          src="src/assets/videos/flirting_1080p.webm"
-          autoPlay
-          loop
-          muted
-        ></video>
-        <div className="flex z-10 animate-slide-left">
-          <img
-            className="w-25 ml-7"
-            src="src/assets/logos/GDC_logo.svg"
-            alt="GDC Logo"
-          />
-          <img
-            className="w-25"
-            src="src/assets/logos/2025-Greenwich-White-Eng.png"
-            alt="UoG Logo"
-          />
-        </div>
-        <div className="flex flex-col z-10 animate-slide-left">
-          <img
-            className="w-125  2xl:w-138"
-            src="src/assets/items/sign_in.webp"
-            alt="Hello"
-          />
-          <p className="ml-7 text-xs w-2/3">
-            Access Greenwich Dance Club member portal to check schedules, connect with members, and stay updated on all GDC activities.
-          </p>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center w-full lg:w-9/13 xl:w-[46%] 2xl:w-2/5 p-10 pl-21 pr-21 sm:pl-51 sm:pr-51 md:pl-33 md:pr-33 lg:pl-24 lg:pr-24 xl:pl-22 xl:pr-22 md:rounded-[6rem] lg:rounded-[5rem] z-10 bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_phone_dọc_full_màn.webp)] sm:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_phone_ngang_full_màn.webp)] md:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_tablet_dọc.webp)] lg:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_tablet_ngang.webp)] xl:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_web.webp)] bg-cover bg-center relative animate-slide-right">
-        
-        {/* Form header */}
-        <Link
-          to="/"
-          className="absolute w-10 top-30 left-1.5 sm:left-7.5 md:left-4.5 lg:left-1.5 m-4 hover:opacity-70"
-        >
-          <img
-            src="src/assets/icons/back_to_home_icon.svg"
-            alt="Back to Home"
-          />
-        </Link>
-        <h1 className="monument-extra-bold text-4xl text-black mb-6 mt-22 2xl:mt-24 w-full">
-          Welcome <br /> Back!
-        </h1>
-
-        {/* Form main */}
-        <form
-          action=""
-          onSubmit={handleSubmit(handleLogin)}
-          className="flex flex-col w-full items-center text-[15px] lg:text-[16px]"
-        >
-          {/* Login with Google buton */}
-          <div className="flex items-center relative w-full">
+    <div className="flex w-full min-h-[var(--pub-main-min-h)] items-center-safe justify-center-safe md:pl-[var(--pub-container-padding-x)] md:pr-[var(--pub-container-padding-x)]">
+      <div className="flex justify-between bg-black w-full md:w-9/12 lg:w-full xl:w-11/12 2xl:w-10/12 rounded-[3rem] relative">
+        <div className="hidden lg:flex lg:flex-col lg:p-10 lg:pt-8 lg:justify-between">
+          <video
+            className="object-cover lg:w-2/3 2xl:w-7/10 h-full absolute top-0 left-0 z-[0] rounded-[3rem] rounded-r-none opacity-75 animate-scale-bigger animate-slide-left"
+            src="src/assets/videos/flirting_1080p.webm"
+            autoPlay
+            loop
+            muted
+          ></video>
+          <div className="flex z-10 animate-slide-left">
             <img
-              className="w-4.5 absolute left-5"
-              src="src/assets/icons/user_icon.svg"
-              alt="User icon"
+              className="w-25 ml-7"
+              src="src/assets/logos/GDC_logo.svg"
+              alt="GDC Logo"
             />
-            <button type="submit" className="bg-[#d0d0d0] text-black p-2 pt-2.5 pb-2.5 pl-14 rounded-[1.25rem] w-1/1 text-left cursor-pointer">
-              Signin with email
-              <span className="text-[var(--pink-color)] ml-1 w-fit">
-                fpt.edu.vn
+            <img
+              className="w-25"
+              src="src/assets/logos/2025-Greenwich-White-Eng.png"
+              alt="UoG Logo"
+            />
+          </div>
+          <div className="flex flex-col z-10 animate-slide-left">
+            <img
+              className="w-125  2xl:w-138"
+              src="src/assets/items/sign_in.webp"
+              alt="Hello"
+            />
+            <p className="ml-7 text-xs w-2/3">
+              Access Greenwich Dance Club member portal to check schedules, connect with members, and stay updated on all GDC activities.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center-safe w-full min-h-[var(--sign-in-form-min-h)] lg:w-9/13 xl:w-[46%] 2xl:w-2/5 p-10 pl-21 pr-21 sm:pl-51 sm:pr-51 md:pl-33 md:pr-33 lg:pl-24 lg:pr-24 xl:pl-22 xl:pr-22 md:rounded-[6rem] lg:rounded-[5rem] z-10 bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_phone_dọc_full_màn.webp)] sm:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_phone_ngang_full_màn.webp)] md:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_tablet_dọc.webp)] lg:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_tablet_ngang.webp)] xl:bg-[url(src/assets/backgrounds/sign_in_background/GDC_background_web.webp)] bg-cover bg-center relative animate-slide-right">
+          
+          {/* Form header */}
+          <div className="flex mb-6 w-full relative">
+            <Link
+              to="/"
+              className="absolute w-10 top-0 left-[-80px] m-4 hover:opacity-70"
+            >
+              <img
+                src="src/assets/icons/back_to_home_icon.svg"
+                alt="Back to Home"
+              />
+            </Link>
+            <h1 className="monument-extra-bold text-4xl text-black w-full">
+              Welcome <br /> Back!
+            </h1>
+          </div>
+
+          {/* Form main */}
+          <form
+            action=""
+            onSubmit={handleSubmit(handleLogin)}
+            className="flex flex-col w-full items-center text-[15px] lg:text-[16px]"
+          >
+            {/* Login with Google buton */}
+            <div className="flex items-center relative w-full">
+              <img
+                className="w-4.5 absolute left-5"
+                src="src/assets/icons/user_icon.svg"
+                alt="User icon"
+              />
+              <button type="submit" className="bg-[#d0d0d0] text-black p-2 pt-2.5 pb-2.5 pl-14 rounded-[1.25rem] w-1/1 text-left cursor-pointer">
+                Signin with email
+                <span className="text-[var(--pink-color)] ml-1 w-fit">
+                  fpt.edu.vn
+                </span>
+              </button>
+            </div>
+
+            <div className="flex items-end text-[14px] w-full mt-4 mb-6 text-[var(--pink-color)] font-medium">
+              <span className="w-[12%] border-b-2 border-[#d0d0d0]"></span>
+              <span className="w-[76%] text-center p-0">
+                OR LOGIN WITH YOUR EMAIL
               </span>
-            </button>
-          </div>
+              <span className="w-[12%] border-b-2 border-[#d0d0d0]"></span>
+            </div>
 
-          <div className="flex items-end text-[14px] w-full mt-4 mb-6 text-[var(--pink-color)] font-medium">
-            <span className="w-[12%] border-b-2 border-[#d0d0d0]"></span>
-            <span className="w-[76%] text-center p-0">
-              OR LOGIN WITH YOUR EMAIL
-            </span>
-            <span className="w-[12%] border-b-2 border-[#d0d0d0]"></span>
-          </div>
+            {/* Email field */}
+            <div className="flex flex-col gap-0.5 mb-1 w-full">
+              <div className="flex flex-col mb-2">
+                <div className="flex items-center relative">
+                  <img
+                    className="w-5 absolute left-5"
+                    src="src/assets/icons/email_icon.svg"
+                    alt="Email icon"
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email Address"
+                    className="p-2 pt-2.5 pb-2.5 pl-14 text-[#454545] bg-[#d0d0d0] rounded-[1.25rem] placeholder-[#454545] w-full focus:outline-none focus:border-[var(--pink-color)] border-2 border-transparent"
+                    {...register('email', { 
+                      required: 'Email cannot be empty',
+                      pattern: {
+                        value: /^[A-Za-z0-9]+@fpt\.edu\.vn$/,
+                        message: 'Email must have @fpt.edu.vn tail'
+                      }
+                    })}
+                  />
+                </div>
+                {/* Email error */}
+                { errors.email &&
+                  <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4">
+                    { errors.email.message }
+                  </span>
+                }
+              </div>
+              
 
-          {/* Email field */}
-          <div className="flex flex-col gap-0.5 mb-1 w-full">
-            <div className="flex flex-col mb-2">
+              {/* Password field */}
               <div className="flex items-center relative">
                 <img
                   className="w-5 absolute left-5"
-                  src="src/assets/icons/email_icon.svg"
-                  alt="Email icon"
+                  src="src/assets/icons/lock_icon.svg"
+                  alt="Lock icon"
                 />
                 <input
-                  type="email"
-                  placeholder="Email Address"
+                  type="password"
+                  placeholder="Password"
                   className="p-2 pt-2.5 pb-2.5 pl-14 text-[#454545] bg-[#d0d0d0] rounded-[1.25rem] placeholder-[#454545] w-full focus:outline-none focus:border-[var(--pink-color)] border-2 border-transparent"
-                  {...register('email', { 
-                    required: 'Email cannot be empty',
-                    pattern: {
-                      value: /^[A-Za-z0-9]+@fpt\.edu\.vn$/,
-                      message: 'Email must have @fpt.edu.vn tail'
+                  {...register('password', { 
+                    required: 'Password cannot be empty',
+                    minLength: {
+                      value: 8,
+                      message: 'Password must be at least 8 characters'
                     }
                   })}
                 />
               </div>
-              {/* Email error */}
-              { errors.email &&
-                <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4">
-                  { errors.email.message }
-                </span>
+
+              { errors.password &&
+                <div className="flex w-full justify-between">
+                  {/* Password error */}
+                    <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4 mb-2">
+                      { errors.password.message }
+                    </span>
+                  <Link
+                    to="/forgot-password"
+                    className="mb-3 self-end text-black text-[10px] lg:text-[11px]"
+                  >
+                    Forgot Password?
+                  </Link>
+                </div>
               }
-            </div>
-            
 
-            {/* Password field */}
-            <div className="flex items-center relative">
-              <img
-                className="w-5 absolute left-5"
-                src="src/assets/icons/lock_icon.svg"
-                alt="Lock icon"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="p-2 pt-2.5 pb-2.5 pl-14 text-[#454545] bg-[#d0d0d0] rounded-[1.25rem] placeholder-[#454545] w-full focus:outline-none focus:border-[var(--pink-color)] border-2 border-transparent"
-                {...register('password', { 
-                  required: 'Password cannot be empty',
-                  minLength: {
-                    value: 8,
-                    message: 'Password must be at least 8 characters'
-                  }
-                })}
-              />
-            </div>
-
-            { errors.password &&
-              <div className="flex w-full justify-between">
-                {/* Password error */}
-                  <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4 mb-2">
-                    { errors.password.message }
-                  </span>
+              { !errors.password && 
                 <Link
                   to="/forgot-password"
-                  className="mb-3 self-end text-black text-[10px] lg:text-[11px]"
+                  className="mb-3 w-full self-end text-black text-[10px] lg:text-[11px] text-end"
                 >
                   Forgot Password?
                 </Link>
-              </div>
+              }
+            </div>
+
+            { isLoading && 
+              <input
+                type="submit"
+                value="SIGN IN"
+                className="monument-regular text-[var(--black-color)] bg-[var(--pink-color)] w-fit pt-[6px] pb-1 pl-8 pr-8 rounded-2xl mt-1.5 mb-52 lg:mb-20 shadow-white-centered font-bold cursor-pointer self-start hover:bg-[var(--dark-pink-color)] hover:text-white"
+                disabled
+              />
             }
 
-            { !errors.password && 
-              <Link
-                to="/forgot-password"
-                className="mb-3 w-full self-end text-black text-[10px] lg:text-[11px] text-end"
-              >
-                Forgot Password?
-              </Link>
+            { !isLoading && 
+              <input
+                type="submit"
+                value="SIGN IN"
+                className="monument-regular text-[var(--black-color)] bg-[var(--pink-color)] w-fit pt-[6px] pb-1 pl-8 pr-8 rounded-2xl mt-1.5 shadow-white-centered font-bold cursor-pointer self-start hover:bg-[var(--dark-pink-color)] hover:text-white"
+              />
             }
-          </div>
-
-          { isLoading && 
-            <input
-              type="submit"
-              value="SIGN IN"
-              className="monument-regular text-[var(--black-color)] bg-[var(--pink-color)] w-fit pt-[6px] pb-1 pl-8 pr-8 rounded-2xl mt-1.5 mb-52 lg:mb-20 shadow-white-centered font-bold cursor-pointer self-start hover:bg-[var(--dark-pink-color)] hover:text-white"
-              disabled
-            />
-          }
-
-          { !isLoading && 
-            <input
-              type="submit"
-              value="SIGN IN"
-              className="monument-regular text-[var(--black-color)] bg-[var(--pink-color)] w-fit pt-[6px] pb-1 pl-8 pr-8 rounded-2xl mt-1.5 mb-52 lg:mb-20 shadow-white-centered font-bold cursor-pointer self-start hover:bg-[var(--dark-pink-color)] hover:text-white"
-            />
-          }
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,6 +1,106 @@
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-[var(--pub-main-min-height)] mt-[var(--pub-main-margin-y)] mb-[var(--pub-main-margin-y)] md:pl-[var(--pub-container-padding-x)] md:pr-[var(--pub-container-padding-x)]">Home Page</div>
+    <div className="flex flex-col min-h-[var(--pub-main-min-height)] mt-[var(--pub-main-margin-y)] mb-[var(--pub-main-margin-y)] w-full md:pl-[var(--pub-container-padding-x)] md:pr-[var(--pub-container-padding-x)]">
+      <div className="flex flex-col bg-[#212121] w-full h-[30rem] rounded-[3rem] p-10 pt-8 pb-6 justify-between">
+        <span className="self-end text-end leading-5 font-light">Lorem ipsum dolor sit amet, consectetuer adipisc-<br />ing elit, sed diam nonummy nibh euismod tincidunt<br /> ut laoreet dolore magna aliquam erat volutpat. Ut<br /> wisi enim ad minim veniam</span>
+
+        <div className="flex items-end justify-between uppercase">
+          <h1 className="monument-extra-bold text-6xl/18">Project:<br /><span className="monument-regular">Stages of Love</span></h1>
+          <button className="monument-regular uppercase leading-10 cursor-pointer">More</button>
+        </div>
+      </div>
+
+      <img className="w-[30rem] m-10 ml-7" src="src/assets/items/activites-items-01.webp" alt="Featured Activities" />
+
+      <div className="grid grid-cols-7 gap-10 w-full">
+        {/* Card 1 */}
+        <div className="flex flex-col w-full h-full bg-[#212121] col-span-4 rounded-[3rem] items-end">
+          <div className="flex flex-col w-[37.5%] h-[30rem] bg-[#121212] rounded-[3rem] rounded-l-none p-5">
+            <h1 className="monument-extra-bold uppercase text-3xl/10"> Mini Project:<br /><span className="monument-regular">Twist</span></h1>
+            <div className="flex flex-col h-full justify-between mt-2">
+              <span className="text-[10.5px] font-light w-10/12">Lorem ipsum dolor sit amet, consec-tetuer adipiscing elit, sed diam nonummy nibh e</span>
+
+              <span className="text-[10.5px] font-light">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euis-mod tincidunt ut laoreet dolore magna ali-quam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing ....</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="flex flex-row w-full h-full bg-[#212121] col-span-3 row-span-9 rounded-[3rem] items-end">
+          <div className="flex flex-col justify-between w-full h-fit bg-[#121212] rounded-[3rem] rounded-t-none p-5">
+            <h1 className="monument-extra-bold uppercase text-3xl/10"> Career Fair<br /><span className="monument-regular">2025</span></h1>
+            <div className="flex flex-row w-full h-full justify-between mt-10">
+              <span className="text-[10.5px] font-light w-[36%]">Lorem ipsum dolor sit amet, consec-tetuer adipiscing elit, sed diam nonummy nibh e</span>
+
+              <span className="text-[10.5px] font-light w-6/12">Lorem ipsum dolor sit amet, consectetuer adipi-scing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing ....</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="flex flex-row w-full h-full bg-[#212121] col-span-4 row-span-22 rounded-[3rem] items-end">
+          <div className="flex flex-col w-full h-fit bg-[#121212] rounded-[3rem] rounded-t-none p-5">
+            <h1 className="monument-extra-bold uppercase text-3xl/10">Photoshoot<br /><span className="monument-regular"></span></h1>
+            <div className="flex flex-row w-3/4 h-full items-end gap-10">
+              <div className="flex flex-col gap-10 w-1/2">
+                <span className="text-[10.5px] font-light w-2/3">Lorem ipsum dolor sit amet, consec-tetuer adipiscing elit, sed diam nonummy nibh e</span>
+                <span className="text-[10.5px] font-light w-full">Lorem ipsum dolor sit amet, consectetuer adipi-scing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing</span>
+              </div>
+
+              <span className="text-[10.5px] font-light w-1/2">Lorem ipsum dolor sit amet, consectetuer adipi-scing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing ....</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 4 */}
+        <div className="flex flex-row w-full h-full bg-[#212121] col-span-3 row-span-14 rounded-[3rem] items-end">
+          <div className="flex flex-col justify-between w-full h-fit bg-[#121212] rounded-[3rem] rounded-t-none p-5">
+            <h1 className="monument-extra-bold uppercase text-3xl/10">Clubfair<br /><span className="monument-regular">2025</span></h1>
+            <div className="flex flex-row w-full h-full justify-between mt-2">
+              <span className="text-[10.5px] font-light w-[36%]">Lorem ipsum dolor sit amet, consec-tetuer adipiscing elit, sed diam nonummy nibh e</span>
+
+              <span className="text-[10.5px] font-light w-6/12">Lorem ipsum dolor sit amet, consectetuer adipi-scing elit, sed diam nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing ....</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 5 */}
+        <div className="flex flex-row w-full h-full bg-[#212121] col-span-3 row-span-3 rounded-[3rem] items-end">
+          <div className="flex flex-row w-full h-fit bg-[#121212] rounded-[3rem] rounded-t-none p-5 pl-9 pr-9 items-end">
+            <div className="flex flex-col h-full justify-between gap-8">
+              <h1 className="monument-extra-bold uppercase text-3xl/10">Student Honoring Ceremony:<br /><span className="monument-regular">Sum25</span></h1>
+              
+              <span className="text-[10.5px] font-light w-9/12">Lorem ipsum dolor sit amet, consectetuer adipi-scing elit, sed diam nonummy nibh e</span>
+            </div>
+            <span className="text-[10.5px] font-light w-[55%]">Lorem ipsum dolor sit amet, consectetuer adipi-scing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat adipi-scing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna ali-quam erat adipiscing ....</span>
+          </div>
+        </div>
+
+        {/* Card 6 */}
+        <div className="flex flex-col w-full h-full bg-[#212121] col-span-4 row-span-2 rounded-[3rem] items-end">
+          <div className="flex flex-col w-[37.5%] bg-[#121212] rounded-[3rem] rounded-l-none p-5">
+            <h1 className="monument-extra-bold uppercase text-3xl/10">Opening Ceremony<br /><span className="monument-regular">2025</span></h1>
+            <div className="flex flex-col h-full justify-between mt-2 gap-12">
+              <span className="text-[10.5px] font-light w-10/12">Lorem ipsum dolor sit amet, consec-tetuer adipiscing elit, sed diam nonummy nibh e</span>
+
+              <span className="text-[10.5px] font-light">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euis-mod tincidunt ut laoreet dolore magna ali-quam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing ....</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Card 7 */}
+        <div className="flex flex-col w-full h-full bg-[#212121] col-span-4 row-span-1 rounded-[3rem] items-start">
+          <div className="flex flex-col w-[37.5%] bg-[#121212] rounded-[3rem] rounded-r-none p-5">
+            <h1 className="monument-extra-bold uppercase text-3xl/10">Casting Call:<br /><span className="monument-regular">Result</span></h1>
+            <div className="flex flex-col h-full justify-between mt-2 gap-24">
+              <span className="text-[10.5px] font-light w-10/12">Lorem ipsum dolor sit amet, consec-tetuer adipiscing elit, sed diam nonummy nibh e</span>
+
+              <span className="text-[10.5px] font-light">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euis-mod tincidunt ut laoreet dolore magna ali-quam erat adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat laoreet dolore magna aliquam erat adipiscing ....</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
