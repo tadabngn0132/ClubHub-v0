f746afe2276f25951a128f86fb705514404157c0
Nguyen Ba Dat <datngba2310@gmail.com>
2026-01-20 12:10:44 +0700

    feat: enhance ForgotPassword and ResetPassword components with improved styling and error handling

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux'
 import { useForm } from "react-hook-form"
 import { forgotPasswordUser } from '../../store/slices/authSlice'
 import { useState } from 'react'
+import { current } from '@reduxjs/toolkit'
 
 const ForgotPassword = () => {
   const dispatch = useDispatch()
@@ -27,26 +28,30 @@ const ForgotPassword = () => {
   }
 
   return (
-    <div>
-      <form action="" onSubmit={handleSubmit(handleForgotPassword)}>
-        <h1>Forgot Password</h1>
-        <label htmlFor="email">Email <span className="text-red-500">*</span></label>
-        <input type="email" id="email" placeholder='Enter your email' {...register('email', {
-          required: 'Email cannot be empty',
-          pattern: {
-            value: /^[A-Za-z0-9]+@fpt\.edu\.vn$/,
-            message: 'Email must have @fpt.edu.vn tail'
-          }
-        })} />
-        {/* Email error */}
-        { errors.email &&
-          <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4">
-            { errors.email.message }
-          </span>
-        }
-        <button type="submit">Submit</button>
+    <div className='flex min-h-[100vh] items-center-safe justify-center-safe'>
+      <form action="" onSubmit={handleSubmit(handleForgotPassword)} className='flex flex-col bg-white/10 w-3/12 p-3.5 rounded-2xl gap-5.5'>
+        <h1 className='text-center text-2xl/tight font-bold w-full'>Forgot Password</h1>
 
-        <p>{ currentResData && currentResData.message }</p>
+        <div className='flex flex-col'>
+          <label htmlFor="email">Email <span className="text-red-500">*</span></label>
+          <input type="email" id="email" className='outline-none border-b-1 pt-2 pb-2 focus:border-b-[var(--pink-color)] text-sm/tight' placeholder='Enter your email' {...register('email', {
+            required: 'Email cannot be empty',
+            pattern: {
+              value: /^[A-Za-z0-9]+@fpt\.edu\.vn$/,
+              message: 'Email must have @fpt.edu.vn tail'
+            }
+          })} />
+          {/* Email error */}
+          { errors.email &&
+            <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] mt-0.5">
+              { errors.email.message }
+            </span>
+          }
+        </div>
+
+        <input type="submit" className='p-1 bg-[var(--pink-color)] w-full rounded-md cursor-pointer hover:bg-[var(--dark-pink-color)]' value="Submit" />
+
+        { currentResData && <p className='p-1.5 text-[var(--pink-color)]'>{ currentResData.message }</p> }
       </form>    
     </div>
   )
--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -49,40 +49,47 @@ const ResetPassword = () => {
   }
   
   return (
-    <div>
+    <div className='flex min-h-[100vh] items-center-safe justify-center-safe'>
       <Toaster position="top-right" reverseOrder={false} />
-      <h1>Reset Password</h1>
-      <form onSubmit={handleSubmit(handleResetPassword)}>
-        <label htmlFor="new-password">New Password <span className="text-red-500">*</span></label>
-        <input type="password" id="new-password" placeholder="Enter your new password" {...register('newPassword', { 
-          required: 'Password cannot be empty',
-          minLength: {
-            value: 8,
-            message: 'Password must be at least 8 characters'
+      <form onSubmit={handleSubmit(handleResetPassword)} className='flex flex-col bg-white/10 w-3/12 p-3.5 rounded-2xl gap-5.5'>
+        <h1 className='text-center text-2xl/tight font-bold w-full'>Reset Password</h1>
+
+        <div className='flex flex-col'>
+          <label htmlFor="new-password">New Password <span className="text-red-500">*</span></label>
+          <input type="password" id="new-password" className='outline-none border-b-1 pt-2 pb-2 focus:border-b-[var(--pink-color)] text-sm/tight' placeholder="Enter your new password" {...register('newPassword', { 
+            required: 'Password cannot be empty',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters'
+            }
+          })} />
+          {/* New Password error */}
+          { errors.newPassword &&
+            <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] mt-0.5">
+              { errors.newPassword.message }
+            </span>
           }
-        })} />
-        {/* New Password error */}
-        { errors.newPassword &&
-          <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4">
-            { errors.newPassword.message }
-          </span>
-        }
-        <label htmlFor="confirm-new-password">Confirm New Password <span className="text-red-500">*</span></label>
-        <input type="password" id="confirm-new-password" placeholder="Confirm your new password" {...register('confirmNewPassword', { 
-          required: 'Password cannot be empty',
-          minLength: {
-            value: 8,
-            message: 'Password must be at least 8 characters'
-          },
-          validate: (value) => value === watch('newPassword') || 'Passwords do not match'
-        })} />
-        {/* Confirm New Password error */}
-        { errors.confirmNewPassword &&
-          <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] pl-4">
-            { errors.confirmNewPassword.message }
-          </span>
-        }
-        <button type="submit">Reset Password</button>
+        </div>
+
+        <div className='flex flex-col'>
+          <label htmlFor="confirm-new-password">Confirm New Password <span className="text-red-500">*</span></label>
+          <input type="password" id="confirm-new-password" className='outline-none border-b-1 pt-2 pb-2 focus:border-b-[var(--pink-color)] text-sm/tight' placeholder="Confirm your new password" {...register('confirmNewPassword', { 
+            required: 'Password cannot be empty',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters'
+            },
+            validate: (value) => value === watch('newPassword') || 'Passwords do not match'
+          })} />
+          {/* Confirm New Password error */}
+          { errors.confirmNewPassword &&
+            <span className="text-[var(--red-color)] text-[10px] lg:text-[11px] mt-0.5">
+              { errors.confirmNewPassword.message }
+            </span>
+          }
+        </div>
+        
+        <input type="submit" className='p-1 bg-[var(--pink-color)] w-full rounded-md cursor-pointer hover:bg-[var(--dark-pink-color)]' value="Reset Password" />
       </form>
     </div>
   )
