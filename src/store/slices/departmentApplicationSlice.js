2e12eef606901ce69c5ea023bdb2dffe0e48e83f
Nguyen Ba Dat <datngba2310@gmail.com>
2026-04-21 15:08:03 +0700

    feat: fix notification preference selector and remove unused createDepartmentApplication import

--- src/store/slices/departmentApplicationSlice.js
+++ src/store/slices/departmentApplicationSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import {
-  createDepartmentApplication,
   getDepartmentApplicationsByMemberApplicationId,
   getDepartmentApplicationById,
   getDepartmentApplications,
